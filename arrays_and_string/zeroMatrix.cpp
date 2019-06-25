019acae2f178a9220c029362f547cd4a065acdc8
theAlchemist398 <akshay.rs3@gmail.com>
2019-06-25 16:45:41 +0530

    Update zeroMatrix.cpp

--- arrays_and_string/zeroMatrix.cpp
+++ arrays_and_string/zeroMatrix.cpp
@@ -49,12 +49,6 @@ void zeroMatrix(vector<vector<int>>& matrix) {
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
         for(int i=1;i<m;i++){
             if(matrix[i][0] == 0) 
                 nullifyRow(matrix, i);
