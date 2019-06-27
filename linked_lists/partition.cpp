2a8bb3755f8d8fa8591715e3b0cca7e121edb929
theAlchemist398 <akshay.rs3@gmail.com>
2019-06-27 18:12:35 +0530

    Update partition.cpp

--- linked_lists/partition.cpp
+++ linked_lists/partition.cpp
@@ -1,1 +1,23 @@
+// Time: O(n)
+// Space: O(1)
+// Maintains relative order of nodes.
 
+ListNode* partition(ListNode* head, int x) {
+    ListNode left(0);
+    ListNode right(0);
+    ListNode* l = &left;
+    ListNode* r = &right;
+    while(head){
+        if(head->val < x){
+            l->next = head;
+            l = l->next;
+        }else{
+            r->next = head;
+            r = r->next;
+        }
+        head = head->next;
+    }
+    l->next = right.next;
+    r->next = NULL;
+    return left.next;
+}
