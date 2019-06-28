301a06a55feab6022dc1ae4495928d6725b70bde
theAlchemist398 <akshay.rs3@gmail.com>
2019-06-28 17:24:22 +0530

    Create intersection.cpp

---
+++ linked_lists/intersection.cpp
@@ -0,0 +1,32 @@
+// Time: O(max(m,n))
+// Space: O(1)
+
+int length(ListNode* head){
+    int n = 0;
+    ListNode* temp = head;
+    while(temp != NULL){
+        n++;
+        temp = temp->next;
+    }
+    return n;
+}
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    int m = length(headA);
+    int n = length(headB);
+    ListNode* temp1 = headA;
+    ListNode* temp2 = headB;
+    while(m > n){
+        temp1 = temp1->next;
+        m--;
+    }
+    while(n > m){
+        temp2 = temp2->next;
+        n--;
+    }
+    while(temp1 && temp2){
+        if(temp1 == temp2) return temp1;
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return NULL;
+}
