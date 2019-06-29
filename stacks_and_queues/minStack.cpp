fbe905a790e3b8e78cfbd0446cd2ea050a5637f8
theAlchemist398 <akshay.rs3@gmail.com>
2019-06-29 13:14:39 +0530

    Create minStack.cpp

---
+++ stacks_and_queues/minStack.cpp
@@ -0,0 +1,32 @@
+// Time: O(1)
+// Space: O(2n) for two stacks.
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> ele;
+    stack<int> min;
+    int size;
+    MinStack() {
+        size = 0;
+    }
+    
+    void push(int x) {
+        ele.push(x);
+        if(min.empty() || x <= min.top()) min.push(x);
+    }
+    
+    void pop() {
+        int x = ele.top();
+        if(!min.empty() && min.top() == x) min.pop();
+        ele.pop();
+    }
+    
+    int top() {
+        return ele.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
