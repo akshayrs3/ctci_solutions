2109c01008ea9134134e3f0515e83514a79ace03
theAlchemist398 <akshay.rs3@gmail.com>
2019-07-02 17:08:52 +0530

    Create LCA.cpp

---
+++ graphs_and_trees/LCA.cpp
@@ -0,0 +1,14 @@
+//Time: O(n)
+// Space: O(h)
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == NULL) return NULL;
+    if(root->val == p->val || root->val == q->val)
+        return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if(left && right) return root;
+    if(left) return left;
+    return right;
+
+}
